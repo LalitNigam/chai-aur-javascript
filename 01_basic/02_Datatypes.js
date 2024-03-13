16cbcd4f7d860f1ed58e7c2cd62c1facc29dca88
Lalit Nigam <lalitnigam46@email.com>
2024-03-13 21:25:57 +0530

    datatypes

---
+++ 01_basic/02_Datatypes.js
@@ -0,0 +1,10 @@
+"use strict";             // treat all js code as newer version
+
+let name = "Lalit Nigam"          //string
+let age = 19                          //number
+let logIn = false                         //boolean
+let state                               //undefine
+let temperature = null                 //null     type of object
+
+
+console.log(typeof temperature)
