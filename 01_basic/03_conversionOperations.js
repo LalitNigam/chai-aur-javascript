b251e7a9645af51ab9a8052523ad97e182f1288b
Lalit Nigam <lalitnigam46@email.com>
2024-03-13 22:54:46 +0530

    conversion datatypes

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,22 @@
+let scor = "46ha"
+console.log(typeof scor)
+
+let convNumber = Number(scor)
+console.log(typeof convNumber)
+
+console.log(convNumber)         
+
+// "33" => 33
+//  "33abc" =>  NaN       (Not a Number)
+// true => 1 ;   false => 0
+
+let logIn = 6
+
+let convBoolean= Boolean(logIn)
+
+console.log(typeof convBoolean)
+console.log( convBoolean)
+
+// 1 => true ;     0 => false
+// "" => false
+// "Lalit" => true
