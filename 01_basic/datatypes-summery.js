37bf52577d2a1295a1e894d269534859d2847582
Lalit Nigam <lalitnigam46@email.com>
2024-03-15 17:42:06 +0530

    datatype summery

---
+++ 01_basic/datatypes-summery.js
@@ -0,0 +1,12 @@
+/*
+         * primitive
+
+         ? 7types  : number, string, boolean,  null, undefined, symbol,  bigInt
+
+        * Reference   (Non primitive)
+
+        ? Array, Object , Function 
+
+
+
+*/
