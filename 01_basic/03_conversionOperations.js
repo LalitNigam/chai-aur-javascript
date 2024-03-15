e5b7406bbb4f7a4a9924d5aee2407a5c8e3a3baf
Lalit Nigam <lalitnigam46@email.com>
2024-03-15 17:14:22 +0530

    oprations

--- 01_basic/03_conversionOperations.js
+++ 01_basic/03_conversionOperations.js
@@ -1,10 +1,10 @@
 let scor = "46ha"
-console.log(typeof scor)
+// *
 
 let convNumber = Number(scor)
-console.log(typeof convNumber)
+// *console.log(typeof convNumber)
 
-console.log(convNumber)         
+// *console.log(convNumber)         
 
 // "33" => 33
 //  "33abc" =>  NaN       (Not a Number)
@@ -14,9 +14,27 @@ let logIn = 6
 
 let convBoolean= Boolean(logIn)
 
-console.log(typeof convBoolean)
-console.log( convBoolean)
+// *console.log(typeof convBoolean)
+// *console.log( convBoolean)
 
 // 1 => true ;     0 => false
 // "" => false
 // "Lalit" => true
+
+//   ************************  opretions *********************************
+
+let value = 3
+let negaValue = -value
+//* console.log(negaValue)
+
+// *console.log(2+2)
+// *console.log(2-2)
+// *console.log(2*2)
+// *console.log(2/3)
+// *console.log(2**2)
+// *console.log(2%3)
+
+let str1= "hello"
+let str2=" Lalit"
+let str3 = str1 + str2
+console.log(str3)
