26a0d2aa9969a4d621a97dd77812f184bf9f36f8
Lalit Nigam <lalitnigam46@email.com>
2024-03-16 17:36:18 +0530

    math and num

---
+++ 01_basic/05_nums_and_math.js
@@ -0,0 +1,31 @@
+const scor = 400
+//*console.log(scor)
+
+const balance = new Number(100)
+//*console.log(balance)
+
+
+//*console.log(balance.toString().length)
+//*console.log(balance.toFixed(2))
+
+const otherNumber = 23.18956
+//*console.log(otherNumber.toPrecision(3))
+
+const hundred = 1000000
+//*console.log(hundred.toLocaleString('en-IN'));
+
+//  +++++++++++++++++++++++++++++++++++++++++   Math s  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// *console.log(Math)
+// *console.log(Math.abs(-5))
+// *console.log(Math.round(4.6))
+// *console.log(Math.ceil(4.2))
+
+// *console.log(Math.random())
+// *console.log(Math.random()*10 + 1)
+// *console.log((Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min )
