b8b47cbbafa9d632ef25a828fc6d617374afa362
Lalit Nigam <lalitnigam46@email.com>
2024-03-16 18:41:28 +0530

    dates

---
+++ 01_basic/06_dates.js
@@ -0,0 +1,14 @@
+let myDate = new Date()
+
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+
+let creatDate = new Date (2024 , 0 , 16 ,5 ,6)
+console.log(creatDate.toLocaleString())
+
+let creatByDate = new Date ("08-03-2024")
+console.log(creatByDate.toLocaleString())
+console.log(creatByDate.getDay())
